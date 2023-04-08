d94438d5f452cee4f3100ff4a7cdd5591589b8eb
ger0nymo <gerokristof0909@gmail.com>
2023-04-08 03:01:24 +0200

    First commit

---
+++ qrhazi/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AzureCosmosDbModule } from '@nestjs/azure-database';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    AzureCosmosDbModule.forRoot({
+      dbName: process.env.AZURE_COSMOS_DB_NAME,
+      endpoint: process.env.AZURE_COSMOS_DB_ENDPOINT,
+      key: process.env.AZURE_COSMOS_DB_KEY,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
