3c64154cc8cee1e0ea0fec47ac8395b37f3eb0a2
ger0nymo <gerokristof0909@gmail.com>
2023-04-10 01:53:46 +0200

    added profile page

---
+++ qrhazi_fe/src/components/BottomNavigationBar.tsx
@@ -0,0 +1,31 @@
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import QrIcon from '@mui/icons-material/QrCode';
+import ProfileIcon from '@mui/icons-material/AccountCircle';
+import Paper from '@mui/material/Paper';
+import React from 'react';
+
+export function BottomNavigationBar(props: any) {
+  return (
+    <Paper
+      sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+      elevation={3}
+    >
+      <BottomNavigation
+        value={props.navigationValue}
+        onChange={props.handleNavigationChange}
+      >
+        <BottomNavigationAction
+          icon={<QrIcon sx={{ fontSize: 35 }} />}
+          label='QR access'
+          value='qr_access'
+        />
+        <BottomNavigationAction
+          icon={<ProfileIcon sx={{ fontSize: 35 }} />}
+          label='Profile'
+          value='profile'
+        />
+      </BottomNavigation>
+    </Paper>
+  );
+}
--- qrhazi_fe/src/components/Home.tsx
+++ qrhazi_fe/src/components/Home.tsx
@@ -9,12 +9,67 @@ import { useNavigate } from 'react-router-dom';
 import { checkToken } from '../api/auth';
 import CircularProgress from '@mui/material/CircularProgress';
 import { retrieveUser, User } from '../api/user';
+import { BottomNavigationBar } from './BottomNavigationBar';
+import { Card, CardContent, CardHeader, CardMedia } from '@mui/material';
+import { MaterialUISwitch } from './MaterialUISwitch';
+import FormControl from '@mui/material/FormControlLabel';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ThemeOptions } from '@mui/material/styles';
+import { Button } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+
+interface MyThemeOptions extends ThemeOptions {
+  canEnterBgColor: string;
+  cantEnterBgColor: string;
+}
+
+const darkMode = localStorage.getItem('isDarkTheme') === 'true';
 
 export default function HomePage() {
   const [isLoading, setLoading] = React.useState(true);
   const [user, setUser] = React.useState<User | null>(null);
+  const [checked, setChecked] = React.useState(darkMode || false);
+  const [navigationValue, setNavigationValue] = React.useState('profile');
+  const [isDarkTheme, setIsDarkTheme] = React.useState(darkMode || false);
+
+  const light: MyThemeOptions = {
+    palette: {
+      mode: 'light',
+    },
+    canEnterBgColor: '#81c784',
+    cantEnterBgColor: '#ff8a80',
+  };
+
+  const dark: MyThemeOptions = {
+    palette: {
+      mode: 'dark',
+    },
+
+    canEnterBgColor: '#2e7d32',
+    cantEnterBgColor: '#d32f2f',
+  };
+
+  const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+    localStorage.setItem('isDarkTheme', event.target.checked.toString());
+    setIsDarkTheme((isDarkTheme) => !isDarkTheme);
+  };
+
+  const handleNavigationChange = (
+    event: React.ChangeEvent<{}>,
+    newValue: string
+  ) => {
+    setNavigationValue(newValue);
+  };
 
   const navigate = useNavigate();
+
+  function signOut() {
+    const cookieString = `token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+    document.cookie = cookieString;
+
+    navigate('/login');
+  }
 
   React.useEffect(() => {
     getUserData();
@@ -31,39 +86,151 @@ export default function HomePage() {
   };
 
   return !isLoading ? (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        minHeight: '100vh',
-      }}
-    >
-      <CssBaseline />
-      <Container component='main' sx={{ mt: 8, mb: 2 }} maxWidth='sm'>
-        <Typography variant='h2' component='h1' gutterBottom>
-          Logged in as {user!.username}
-        </Typography>
-        <Typography variant='h5' component='h2' gutterBottom>
-          {user!.email}
-        </Typography>
-        <Typography variant='body1'>
-          {' '}
-          {user!.canEnter ? 'Can enter' : "Can't enter"}
-        </Typography>
-      </Container>
-      <Box
-        component='footer'
-        sx={{
-          py: 3,
-          px: 2,
-          mt: 'auto',
-          backgroundColor: (theme) =>
-            theme.palette.mode === 'light'
-              ? theme.palette.grey[200]
-              : theme.palette.grey[800],
-        }}
-      ></Box>
-    </Box>
+    navigationValue === 'profile' ? (
+      <ThemeProvider
+        theme={isDarkTheme ? createTheme(dark) : createTheme(light)}
+      >
+        <Box
+          sx={{
+            backgroundColor: isDarkTheme ? '#121212' : '#fff',
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100vh',
+          }}
+        >
+          <Container component='main' sx={{ mt: 3, mb: 2 }} maxWidth='sm'>
+            <Card>
+              <CardContent>
+                <Typography
+                  variant='subtitle1'
+                  component='div'
+                  sx={{
+                    flex: '0 0 auto',
+                    marginRight: '5px',
+                    color: '#707070',
+                    fontSize: '1.1rem',
+                    userSelect: 'none',
+                  }}
+                >
+                  Logged in as
+                </Typography>
+                <Typography
+                  variant='h3'
+                  component='div'
+                  sx={{ flex: '1 1 auto', userSelect: 'none' }}
+                >
+                  {user?.username}
+                </Typography>
+                <Typography
+                  variant='body2'
+                  sx={{
+                    fontStyle: 'italic',
+                    mt: 1,
+                    textAlign: 'right',
+                    userSelect: 'none',
+                  }}
+                >
+                  ({user?.email})
+                </Typography>
+              </CardContent>
+            </Card>
+
+            {user?.canEnter === true ? (
+              <Card
+                sx={{
+                  mt: 2,
+                  backgroundColor: `${
+                    isDarkTheme ? dark.canEnterBgColor : light.canEnterBgColor
+                  }`,
+                }}
+              >
+                <CardContent sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Typography
+                    variant='subtitle1'
+                    sx={{ mt: 1, fontSize: '1.1rem', userSelect: 'none' }}
+                  >
+                    You <b>have</b> access to enter.
+                  </Typography>
+                </CardContent>
+              </Card>
+            ) : (
+              <Card
+                sx={{
+                  mt: 2,
+                  backgroundColor: `${
+                    isDarkTheme ? dark.cantEnterBgColor : light.cantEnterBgColor
+                  }`,
+                }}
+              >
+                <CardContent sx={{ display: 'flex', alignItems: 'center' }}>
+                  <Typography
+                    variant='subtitle1'
+                    sx={{ mt: 1, fontSize: '1.1rem', userSelect: 'none' }}
+                  >
+                    You <b>do not have</b> access to enter.
+                  </Typography>
+                </CardContent>
+              </Card>
+            )}
+            <Card sx={{ mt: 2 }}>
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item xs={6} sx={{ mt: 1 }}>
+                    <Typography variant='h5' sx={{ userSelect: 'none' }}>
+                      Dark mode
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6} sx={{ textAlign: 'right' }}>
+                    <MaterialUISwitch
+                      sx={{ mt: 1 }}
+                      checked={checked}
+                      onChange={handleSwitchChange}
+                    />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+            <Card sx={{ mt: 2 }}>
+              <CardContent>
+                <Grid container spacing={2}>
+                  <Grid item xs={6} sx={{ mt: 1 }}>
+                    <Typography variant='h5' sx={{ userSelect: 'none' }}>
+                      Sign out
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={6} sx={{ textAlign: 'right' }}>
+                    <Button
+                      variant='contained'
+                      endIcon={<LogoutIcon />}
+                      sx={{ mt: 1 }}
+                      onClick={() => {
+                        setUser(null);
+                        signOut();
+                      }}
+                    >
+                      Sign out
+                    </Button>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Container>
+
+          <BottomNavigationBar
+            handleNavigationChange={handleNavigationChange}
+            navigationValue={navigationValue}
+          />
+        </Box>
+      </ThemeProvider>
+    ) : (
+      <>
+        <p>asd</p>
+        <BottomNavigationBar
+          handleNavigationChange={handleNavigationChange}
+          navigationValue={navigationValue}
+        />
+      </>
+    )
   ) : (
     <Grid
       sx={{
