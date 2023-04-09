deb0c6821d9c3366ca939ea788044f5a8fed7829
ger0nymo <gerokristof0909@gmail.com>
2023-04-09 18:12:12 +0200

    enabled cors

--- qrhazi_fe/src/api/auth.ts
+++ qrhazi_fe/src/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/auth';
+const API_URL = 'https://iot-qr-hw-u9bn.vercel.app/auth';
 
 export async function register(
   username: string,
