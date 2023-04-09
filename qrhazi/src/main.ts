d428e582cefbbf25fd072519779544bb2258151f
ger0nymo <gerokristof0909@gmail.com>
2023-04-09 18:39:44 +0200

    asd

--- qrhazi/src/main.ts
+++ qrhazi/src/main.ts
@@ -3,8 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3001);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  await app.listen(3000);
 }
 bootstrap();
